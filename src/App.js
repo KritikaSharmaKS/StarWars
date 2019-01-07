f9ef768872474d28db75a4c325d2d021a8821588
KritikaSharmaKS <kritikasharma@Kritikas-MacBook-Pro.local>
2019-01-07 10:47:56 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+		super()
+		this.state = {
+			users: []
+		}
+	}
+
+
+  componentDidMount(){
+    let initialUsers = [];
+    console.log("componentDidMount");
+    fetch('https://swapi.co/api/people/')
+		.then(response => response.json())
+    .then(people => {
+      initialUsers = people.results.map(p => p);
+      this.setState({ users: initialUsers, });
+      console.log(initialUsers);
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    // console.log("render");
+    //const { users } = this.state.users;
+  //   return (
+  //     <div>
+  //     <h1>{'This will always render'}</h1>
+  //     { this.state && this.state.users &&
+  //         <div>{'This will just render after the return of the async call'}
+  //         {this.state.users.map(i => <li>{i.name}</li>)}
+  //         </div>
+  //     }
+  //     </div>
+  // )
+    return this.state.users ? (
+      <div>
+        <h1>Star Wars</h1>
+        { this.state && this.state.users &&
+          <div>
+            {this.state.users.map(i => <li>{i.name}</li>)}
+          </div>
+      }
       </div>
-    );
+    ) : <h1>Loading</h1>
   }
 }
 

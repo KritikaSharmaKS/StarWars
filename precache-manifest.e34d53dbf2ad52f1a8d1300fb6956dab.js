0a3d6b465922567bd39eb6f49f51efe52109e997
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-09 20:23:34 -0500

    Updates

---
+++ precache-manifest.e34d53dbf2ad52f1a8d1300fb6956dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c38304c028a186cc9ba",
+    "url": "/StarWars/static/js/runtime~main.0c38304c.js"
+  },
+  {
+    "revision": "1159ae72d593d3ee9e6d",
+    "url": "/StarWars/static/js/main.1159ae72.chunk.js"
+  },
+  {
+    "revision": "5e4d8245ed5656cf5088",
+    "url": "/StarWars/static/js/1.5e4d8245.chunk.js"
+  },
+  {
+    "revision": "1159ae72d593d3ee9e6d",
+    "url": "/StarWars/static/css/main.27c595e7.chunk.css"
+  },
+  {
+    "revision": "5e4d8245ed5656cf5088",
+    "url": "/StarWars/static/css/1.25555e4b.chunk.css"
+  },
+  {
+    "revision": "41a6f53afd38bf3bb7453f1749933fe9",
+    "url": "/StarWars/index.html"
+  }
+];
